77da59f9faf02440229b29e7d4f17a32490b65e7
shekhAzharuddin <talktoskazhar786@gmail.com>
2026-05-23 14:07:07 +0530

    final commit

---
+++ 01_viteReact/viteReact/src/App.jsx
@@ -0,0 +1,14 @@
+import Name from './name.jsx'
+
+
+function App() {
+
+
+  return (
+   <>
+   <Name/>
+   </>
+  )
+}
+
+export default App
---
+++ 01_viteReact/viteReact/src/name.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+function Name(){
+    return(
+    <>
+    <h1>hello i am Shekh Azharuddin</h1>
+    </>
+)}
+
+export default Name;
